f1bc43aeb3d2ee85799ca7eed76f1dcd8119d544
tum <hongsachm66.gmail.com>
2023-09-17 19:05:02 +0700

    tum'swork

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,19 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter"
+let lastname = "Anderson"
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
 
-//Exercise #2: Variable Reassignment
-//Start coding here
+const restaurantName = "Chef's Table by Chef James"
+const totalprice = 15000;
+const AllactiveRestaurant = 1500;
+// Exercise #1: Variable Creation 
+// Start coding here 
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalPrice = 5000;
+allActiveRestaurant = 2500;
+// Exercise #2: Variable Reassignment
+// Start coding here
+
+
+
